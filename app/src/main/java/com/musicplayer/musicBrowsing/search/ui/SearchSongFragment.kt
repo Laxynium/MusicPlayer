3561d0e66d8339529c0de5ee38a1afd7d7f3147d
Grzegorz Gruszczyk <grzesiu15432@gmail.com>
2020-05-16 15:54:11 +0200

    Add data binding

--- app/src/main/java/com/musicplayer/musicBrowsing/search/ui/SearchSongFragment.kt
+++ app/src/main/java/com/musicplayer/musicBrowsing/search/ui/SearchSongFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.musicplayer.R
+import com.musicplayer.databinding.FragmentSearchSongBinding
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class SearchSongFragment : Fragment() {
@@ -15,6 +17,8 @@ class SearchSongFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val binding = DataBindingUtil.setContentView<FragmentSearchSongBinding>(this.requireActivity(),R.layout.fragment_search_song)
+        binding.viewModel = viewModel
         return inflater.inflate(R.layout.fragment_search_song, container, false)
     }
 }
--- app/src/main/java/com/musicplayer/musicBrowsing/search/ui/SearchSongViewModel.kt
+++ app/src/main/java/com/musicplayer/musicBrowsing/search/ui/SearchSongViewModel.kt
@@ -1,11 +1,13 @@
 package com.musicplayer.musicBrowsing.search.ui
 
+import androidx.databinding.Bindable
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.musicplayer.framework.MessageBus
+import com.musicplayer.ui.ObservableViewModel
 
-class SearchSongViewModel(private val messageBus: MessageBus):ViewModel(){
+class SearchSongViewModel(private val messageBus: MessageBus): ObservableViewModel() {
 
+    @Bindable
     val searchText = MutableLiveData<String>()
 
     fun search() {
